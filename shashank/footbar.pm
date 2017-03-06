e950f11a1f56d844859a7ee4b1eb9c3903fb080e
Shashank Pathak <shashank@technion.ac.il>
2017-03-06 16:46:16 +0200

    check a personal homepage in simpleHTML format

---
+++ shashank/footbar.pm
@@ -0,0 +1,1 @@
+<p><a href="index.html">Home</a>|<a href="education.html">Education</a>|<a href="research.html">Research</a>|<a href="personal.html">Personal</a>|<a href="contact.html">Contact</a></p>
---
+++ shashank/sidebar.pm
@@ -0,0 +1,16 @@
+<p><a href="homepage.html">Home</a>
+<a href="education.html">Education</a>
+<a href="research.html">Research </a>
+<a href="software.html">Software</a>
+<a href="publications.html">Publications</a>
+
+<hr />
+
+<a href="personal.html">Personal</a>
+
+<hr />
+
+<a href="contact.html">Contact</a>
+<a href="credits.html">Credits</a></p>
+
+
