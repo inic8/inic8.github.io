3e5524e201f157ccaacd19f0de9b74cb054e9a2d
shashank <shashank@transabel.com>
2021-07-10 16:43:34 +0200

    remove all

--- shashank/footbar.pm
+++
@@ -1,1 +0,0 @@
-<p><a href="index.html">Home</a>|<a href="education.html">Education</a>|<a href="research.html">Research</a>|<a href="personal.html">Personal</a>|<a href="contact.html">Contact</a></p>
